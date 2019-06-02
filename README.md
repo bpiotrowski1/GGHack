6af54ab9aa6359ad8bc98561067ad3d713a59ccd
bpiotrowski1 <baej.p26@gmail.com>
2019-06-02 12:36:39 +0200

    final commit

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Autor : Błażej Piotrowski
 
-
+### Problem:
+##### https://pl.spoj.com/problems/JGGHACK/
 ### Budowanie projektu : 
 ##### git clone https://github.com/bpiotrowski1/GGHack.git
 ##### cd GGHack/
--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanStr = new Scanner(System.in);
+        String pass;
 
-
+        while ((pass = scanStr.nextLine()) != null) {
+            for (int i = 0; i < pass.length(); i += 2) {
+                char letter1 = pass.charAt(i), letter2 = pass.charAt(i + 1);
+                int realValue = ((int) letter1 - 65 + ((int)letter2-65)*16);
+                System.out.print((char)realValue);
+            }
+        }
+    }
 }
